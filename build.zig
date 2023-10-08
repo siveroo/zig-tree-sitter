d71ae72d9dc4c68bd95d02e7baabf20f0c17c66b
Dwi Andhika <dwiandhikaaji@gmail.com>
2023-10-08 15:15:57 +0700

    update build system to zig 0.11.0

--- build.zig
+++ build.zig
@@ -1,32 +1,37 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) !void {
-    const root = comptime std.fs.path.dirname(@src().file) orelse ".";
-    const mode = b.standardReleaseOptions();
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary("zig-tree-sitter", root ++ "/src/lib.zig");
-    lib.use_stage1 = true;
-    lib.setBuildMode(mode);
+    const lib = b.addStaticLibrary(.{
+        .name = "zig-tree-sitter",
+        .target = target,
+        .optimize = optimize,
+        .root_source_file = .{ .path = "src/lib.zig" },
+    });
     linkTreeSitter(lib);
-    lib.install();
+    b.installArtifact(lib);
 
-    var main_tests = b.addTest(root ++ "/src/tests.zig");
-    main_tests.use_stage1 = true;
+    var main_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/tests.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     main_tests.linkLibrary(lib);
-    main_tests.addIncludePath(root ++ "/tree-sitter-json/src");
-    main_tests.addCSourceFile(root ++ "/tree-sitter-json/src/parser.c", &[_][]const u8 {});
-    main_tests.setBuildMode(mode);
+    main_tests.addIncludePath(.{ .path = "tree-sitter-json/src" });
+    main_tests.addCSourceFile(.{ .file = .{ .path = "tree-sitter-json/src/parser.c" }, .flags = &.{} });
+
+    const run_main_tests = b.addRunArtifact(main_tests);
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&lib.step);
-    test_step.dependOn(&main_tests.step);
+    test_step.dependOn(&run_main_tests.step);
 }
 
-pub fn linkTreeSitter(exe: *std.build.LibExeObjStep) void {
-    const root = comptime std.fs.path.dirname(@src().file) orelse ".";
-
+pub fn linkTreeSitter(exe: *std.build.Step.Compile) void {
     exe.linkLibC();
-    exe.addIncludePath(root ++ "/tree-sitter/lib/src");
-    exe.addIncludePath(root ++ "/tree-sitter/lib/include");
-    exe.addCSourceFile(root ++ "/tree-sitter/lib/src/lib.c", &[_][]const u8 {});
+    exe.addCSourceFile(.{ .file = .{ .path = "tree-sitter/lib/src/lib.c" }, .flags = &.{} });
+    exe.addIncludePath(.{ .path = "tree-sitter/lib/src" });
+    exe.addIncludePath(.{ .path = "tree-sitter/lib/include" });
 }
